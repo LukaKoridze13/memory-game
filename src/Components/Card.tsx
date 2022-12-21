9e53aebdcf880c7357a1e0e876035caecf1f6365
Luka Koridze <lukakoridze13@gmail.com>
2022-12-21 20:38:40 +0400

    Modify Icon Size

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -3,6 +3,6 @@ import React from "react";
 export default function Card(props: {id:number, content: number | JSX.Element, checked: boolean, opened: boolean, onClick: (id:number,checked:boolean, opened:boolean)=> void }) {
     const {id,content,checked,opened, onClick} = props
   return (
-    <div onClick={()=>{onClick(id,checked,opened)}} className={`${opened && '!bg-lightgrey'} ${opened && 'pointer-events-none'} ${checked && '!bg-orange'} w-full rounded-full aspect-square bg-darkgrey flex items-center justify-center font-bold text-darkwhite text-xs cursor-pointer md:text-4xl hover:bg-bluegrey`}>{(checked || opened) && content} {content}</div>
+    <div onClick={()=>{onClick(id,checked,opened)}} className={`${opened && '!bg-lightgrey'} ${opened && 'pointer-events-none'} ${checked && '!bg-orange'} w-full rounded-full aspect-square bg-darkgrey flex items-center justify-center font-bold text-darkwhite text-2xl cursor-pointer md:text-5xl md:hover:bg-bluegrey`}>{(checked || opened) && content}</div>
   );
 }
